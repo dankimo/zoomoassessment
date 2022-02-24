932254530194fba8449e12c9c018b6b12bf23a21
Dan Kimmorley <dan@thinksmartit.com.au>
2022-02-24 11:52:48 +1000

    added + tested code for question1

--- ZoomoAssessment.Tests/Question1.cs
+++ ZoomoAssessment.Tests/Question1.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using ZoomoAssessment;
+﻿using NUnit.Framework;
+using static ZoomoAssessment.Question1;
 
 namespace ZoomoAssessment.Tests
 {
--- ZoomoAssessment/Question1.cs
+++ ZoomoAssessment/Question1.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ZoomoAssessment
 {
-    public class Question1
+    public static class Question1
     {
+        public static bool isNullorEmpty(string input)
+        {
+            if (input == null)
+            {
+                return true;
+            }
+
+            if (input.Length == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
