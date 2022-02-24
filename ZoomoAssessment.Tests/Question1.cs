a7fbf16cc102eb3b0e9d96976c5a4bda176039fa
Dan Kimmorley <dan@thinksmartit.com.au>
2022-02-24 11:49:41 +1000

    added question1 tests

---
+++ ZoomoAssessment.Tests/Question1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ZoomoAssessment;
+
+namespace ZoomoAssessment.Tests
+{
+    [TestFixture]
+    public class Question1
+    {
+        [Test]
+        public void isNullorEmpty_Null_ReturnsTrue()
+        {
+            Assert.AreEqual(isNullorEmpty(null), true);
+        }
+
+        [Test]
+        public void isNullorEmpty_ContainsCharacter_ReturnsFalse()
+        {
+            Assert.AreEqual(isNullorEmpty("a"), false);
+        }
+
+        [Test]
+        public void isNullorEmpty_Empty_ReturnsTrue()
+        {
+            Assert.AreEqual(isNullorEmpty(""), true);
+        }
+
+        [Test]
+        public void isNullorEmpty_NullLiteral_ReturnsFalse()
+        {
+            Assert.AreEqual(isNullorEmpty("null"), false);
+        }
+    }
+}
--- ZoomoAssessment.Tests/Question3.cs
+++ ZoomoAssessment.Tests/Question3.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace ZoomoAssessment.Tests
 {
-    public class Class1
+    class Question3
     {
     }
 }
